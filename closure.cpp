bb0202cfb0cc02e29a0042b1eabce5eadcf2822f
O_o <>
2019-05-30 01:34:28 +0200

    chg: internal closure layout

--- closure.cpp
+++ closure.cpp
@@ -9,9 +9,9 @@ static Expr macro_tag     = nil;
 
 // TODO sync closure.lisp, otherwise interop may break
 // (we could also try to reuse the structure of the existing lambda/syntax/defun/defmacro forms
-// (lambda args stmt1 stmt2 stmt3) => (tag name args stmt1 stmt2 stmt3)
+// (lambda args stmt1 stmt2 stmt3) => ((tag . env) name args stmt1 stmt2 stmt3)
 // where everything from args onwards is reused => change API and profile
-// (defun name args stmt1 stmt2) => (tag name args stmt1 stmt2)
+// (defun name args stmt1 stmt2) => ((tag . env) name args stmt1 stmt2)
 // where everything from name onwards is reused
 static Expr make_closure(Expr tag, Expr env, Expr params, Expr body, Expr name)
 {
@@ -20,7 +20,7 @@ static Expr make_closure(Expr tag, Expr env, Expr params, Expr body, Expr name)
 #else
     // TODO profile/reorder/optimize by access pattern?
     // TODO maybe a vector is a better idea for struct-like types?
-    return list(tag, env, params, body, name);
+    return cons(cons(tag, env), cons(name, cons(params, body)));
 #endif
 }
 
@@ -31,7 +31,9 @@ static Bool is_tagged(Expr exp, Expr tag)
         is_cons(car(exp)) &&
         caar(exp) == tag;
 #else
-    return is_cons(exp) && car(exp) == tag;
+    return is_cons(exp) &&
+        is_cons(car(exp)) &&
+        caar(exp) == tag;
 #endif
 }
 
@@ -63,7 +65,7 @@ Expr closure_env(Expr closure)
 #if LEGACY
     return cdar(closure);
 #else
-    return cadr(closure); // TODO use nth
+    return cdar(closure);
 #endif
 }
 
@@ -72,7 +74,7 @@ Expr closure_params(Expr closure)
 #if LEGACY
     return cadr(closure);
 #else
-    return caddr(closure); // TODO use nth
+    return caddr(closure);
 #endif
 }
 
@@ -81,7 +83,7 @@ Expr closure_body(Expr closure)
 #if LEGACY
     return cddr(closure);
 #else
-    return cadddr(closure); // TODO use nth
+    return cdddr(closure);
 #endif
 }
 
@@ -90,7 +92,7 @@ Expr closure_name(Expr closure)
 #if LEGACY
     return nil; // we don't store names!
 #else
-    return caddddr(closure); // TODO use nth
+    return cadr(closure);
 #endif
 }
 
