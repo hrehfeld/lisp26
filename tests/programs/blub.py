a24966840a97408ed0b82aa73d1be0707d4bc56f
O_o <>
2019-05-31 14:00:33 +0200

    add: compiled version of blub.lisp and hello.lisp

---
+++ tests/programs/blub.py
@@ -0,0 +1,26 @@
+
+def foo():
+    print(1)
+
+def bar():
+    print(2)
+    print(3)
+
+def omfg():
+    if not happy:
+        show_it()
+
+if None:
+    pass
+
+def empty():
+    pass
+
+def fib(n):
+    if n < 2:
+        return n
+    else:
+        return fib(n - 2) + fib(n - 1)
+
+if __name__ == "__main__":
+    print("Hello from lisp")
