90f8936be7743b83dea1d9052f7417b1eb64144c
O_o <>
2019-06-08 14:58:09 +0200

    add: options to disable core/sdl2 bindings

--- lisp.cpp
+++ lisp.cpp
@@ -38,9 +38,17 @@ static int lisp_main(int argc, char ** argv)
                 {
                     load_core = 1;
                 }
+                else if (!strcmp("--no-core", argv[i]))
+                {
+                    load_core = 0;
+                }
                 else if (!strcmp("--sdl2", argv[i]))
                 {
                     load_sdl2 = 1;
+                }
+                else if (!strcmp("--no-sdl2", argv[i]))
+                {
+                    load_sdl2 = 0;
                 }
                 else if (!strcmp("--show-pass", argv[i]))
                 {
@@ -80,7 +88,7 @@ static int lisp_main(int argc, char ** argv)
 
         if (load_core)
         {
-            system_bind_core(env); // TODO allow user to disable this?
+            system_bind_core(env);
         }
 
         if (load_sdl2)
